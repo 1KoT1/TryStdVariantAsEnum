ab29ec23f08b1b6db47dc59c86d598e06e216f51
Василий Почкаенко <pochkaenkov@yandex.ru>
2018-11-08 11:05:58 +0700

    Make case with unhandled enum value.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 #include <stdexcept>
 
+#define NEW_ENUM_VALUE
+
 using namespace std;
 
 enum State {
 	Ready,
 	Running
+#ifdef NEW_ENUM_VALUE
+	,Preparation
+#endif
 };
 
 int main() {
+#ifdef NEW_ENUM_VALUE
+	State s(State::Preparation);
+#else
 	State s(State::Ready);
+#endif
 
 	switch (s) {
 		case State::Ready:
